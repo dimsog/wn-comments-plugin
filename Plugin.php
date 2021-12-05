a1ef640136f5c3fa28bd71462cd0f43b71e2de2f
Dmitry R <dimsog@gmail.com>
2021-12-05 20:45:36 +0300

    Formatting

--- Plugin.php
+++ Plugin.php
@@ -1,19 +1,14 @@
-<?php namespace Dimsog\Comments;
+<?php
+
+namespace Dimsog\Comments;
 
 use Backend;
 use System\Classes\PluginBase;
 
-/**
- * Comments Plugin Information File
- */
+
 class Plugin extends PluginBase
 {
-    /**
-     * Returns information about this plugin.
-     *
-     * @return array
-     */
-    public function pluginDetails()
+    public function pluginDetails(): array
     {
         return [
             'name'        => 'Comments',
@@ -23,74 +18,8 @@ class Plugin extends PluginBase
         ];
     }
 
-    /**
-     * Register method, called when the plugin is first registered.
-     *
-     * @return void
-     */
-    public function register()
+    public function registerNavigation(): array
     {
-
-    }
-
-    /**
-     * Boot method, called right before the request route.
-     *
-     * @return array
-     */
-    public function boot()
-    {
-
-    }
-
-    /**
-     * Registers any front-end components implemented in this plugin.
-     *
-     * @return array
-     */
-    public function registerComponents()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'Dimsog\Comments\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * Registers any back-end permissions used by this plugin.
-     *
-     * @return array
-     */
-    public function registerPermissions()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'dimsog.comments.some_permission' => [
-                'tab' => 'Comments',
-                'label' => 'Some permission'
-            ],
-        ];
-    }
-
-    /**
-     * Registers back-end navigation items for this plugin.
-     *
-     * @return array
-     */
-    public function registerNavigation()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'comments' => [
-                'label'       => 'Comments',
-                'url'         => Backend::url('dimsog/comments/mycontroller'),
-                'icon'        => 'icon-leaf',
-                'permissions' => ['dimsog.comments.*'],
-                'order'       => 500,
-            ],
-        ];
+        return [];
     }
 }
