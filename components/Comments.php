1d7ad4900844bb29f5e3d915ebde743326bb1cdc
Dmitry R <dimsog@gmail.com>
2021-12-05 21:16:23 +0300

    Init Comments component

---
+++ components/Comments.php
@@ -0,0 +1,19 @@
+<?php namespace Dimsog\Comments\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Comments extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Comments Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
