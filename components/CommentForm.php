c3480d19955b5858a5d0c3f7eadbf1494093103a
Dmitry R <dimsog@gmail.com>
2021-12-05 21:13:53 +0300

    Init CommentForm component

---
+++ components/CommentForm.php
@@ -0,0 +1,19 @@
+<?php namespace Dimsog\Comments\Components;
+
+use Cms\Classes\ComponentBase;
+
+class CommentForm extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'CommentForm Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
