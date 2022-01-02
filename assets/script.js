0cd6c1d56364954be2f6cdbd63c13b254eb96568
Dmitry R <dimsog@gmail.com>
2022-01-02 19:20:16 +0300

    Replace id to class

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,10 @@ $(function () {
     $(document).on('click', '.app-dimsog-comment-item-answer__link', function (e) {
         e.preventDefault();
 
-        var $form = $('#app-dimsog-comment-form');
+        var $root = $(this).closest('.dimsog-comments-wrapper');
+        var $form = $root.find('form');
         var $parentIdInput = $form.find('input[name=parent_id]');
-        var $answerToBlock = $form.find('#app-dimsog-comment-form-answer-block');
+        var $answerToBlock = $form.find('.app-dimsog-comment-form-answer-block');
 
         $parentIdInput.val($(this).data('comment-id'));
         $answerToBlock.html('<div class="dimsog-comment-form-field dimsog-comment-form-field--reply-user"><i class="icon-reply"></i> ' + e.target.dataset.userName + '</div>');
