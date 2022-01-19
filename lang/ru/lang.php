7533075a977f034fcb652808c83364eb17e7008e
Dmitry R <dimsog@gmail.com>
2022-01-19 19:33:50 +0300

    Russian support

---
+++ lang/ru/lang.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Комментарии',
+        'description' => 'Нет описания'
+    ],
+    'components' => [
+        'comments' => [
+            'name' => 'Список комментариев',
+            'description' => '',
+            'properties' => [
+                'url' => 'URL',
+                'tree' => 'Дерево комментариев',
+                'email' => 'Показывать поле Email',
+                'dateformat' => 'Формат даты'
+            ],
+            'success_message_moderate' => 'Ваш комментарий будет опубликован для всех после проверки',
+            'success_message' => 'Комментарий добавлен',
+            'views' => [
+                'empty' => 'Нет комментариев',
+                'deleted' => 'Комментарий удален',
+                'answer' => 'Ответить',
+                'your_name' => 'Ваше имя',
+                'your_email' => 'Ваш email',
+                'text' => 'Текст комментария',
+                'send' => 'Отправить'
+            ]
+        ]
+    ],
+    'settings' => [
+        'name' => 'Комментарии',
+        'moderate' => 'Публиковать комментарии после модерации'
+    ],
+    'models' => [
+        'comment' => [
+            'columns' => [
+                'id' => 'ID',
+                'created_at' => 'Дата создания',
+                'user_name' => 'Имя',
+                'user_email' => 'Email',
+                'url' => 'URL',
+                'active' => 'Опубликовано',
+                'deleted' => 'Удалено'
+            ],
+            'fields' => [
+                'user_name' => 'Имя пользователя',
+                'user_email' => 'Email',
+                'comment' => 'Текст комментария'
+            ]
+        ]
+    ]
+];
