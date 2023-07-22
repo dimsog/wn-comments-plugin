b587c402efde7b557d30f3416a3a5e2a1f8e73c2
Dmitry R <dimsog@gmail.com>
2023-07-22 21:19:04 +0300

    fix: check querySelector is not null

--- assets/src/js/helpers/htmlToDom.js
+++ assets/src/js/helpers/htmlToDom.js
@@ -1,5 +1,5 @@
 export default function (htmlMarkup) {
     const $template = document.createElement('template');
     $template.innerHTML = htmlMarkup;
-    return $template.content.firstChild;
+    return $template.content;
 }
